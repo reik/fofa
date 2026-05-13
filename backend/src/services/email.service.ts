dd2c0dfabd67b866d865cbe3467fe9e7d37ee945
Rei Kurata <kurarei@gmail.com>
2026-05-13 09:22:18 -0700

    fix smtp

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -5,7 +5,6 @@ function createTransport() {
     host: 'smtp.gmail.com',
     port: 587,
     secure: false,
-    family: 4,
     auth: {
       user: process.env.GMAIL_USER,
       pass: process.env.GMAIL_APP_PASSWORD,
